e2eef5d269f0d65d393a74991465cd5d886035da
reuben-k-o <rubenkhaemba@gmail.com>
2022-06-10 21:10:05 +0300

    managing state

--- src/components/NewGoal/NewGoal.js
+++ src/components/NewGoal/NewGoal.js
@@ -2,9 +2,21 @@ import React from "react";
 
 import "./NewGoal.css";
 
-const NewGoal = () => {
+const NewGoal = (props) => {
+  const addGoalHandler = (event) => {
+    event.preventDefault();
+
+    const newGoal = {
+      id: Math.random().toString(),
+      text: "My new goal",
+    };
+
+    console.log(newGoal);
+    props.onAddGoal(newGoal);
+  };
+
   return (
-    <form className="new-goal">
+    <form className="new-goal" onSubmit={addGoalHandler}>
       <input type="text" />
       <button type="submit">Add Goal</button>
     </form>
